1304af4115f61d77305625c7abb1ba008686fb3e
Vitaliy Novikov <novikov.vitaliy.2014@gmail.com>
2019-07-17 22:50:26 +0300

    store

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,9 @@
 import Vue from "vue"
 import Vuex from "vuex"
-import axios from './../axios-auth'
-import globalAxios from 'axios'
-import * as firebase from 'firebase'
-import router from '../router.js'
+// import axios from './../axios-auth'
+// import globalAxios from 'axios'
+// import * as firebase from 'firebase'
+// import router from '../router.js'
 import outlay from './modules/outlay.js'
 import user from './modules/user.js'
 import * as actions from './actions.js'
